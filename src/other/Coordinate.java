73319e6f49f769dfc3d46b21009bedc436decc3e
Brandt Newton <btnewton@uvm.edu>
2015-02-03 21:30:29 -0500

    added jdocs and english help

--- src/other/Coordinate.java
+++ src/other/Coordinate.java
@@ -1,23 +1,44 @@
 package other;
 
+/**
+ * Coordinate class is used to store a coordinate.
+ * 
+ * Coordinates include a row position and a column position. 
+ * Note: for use in a 2D array: array[row][column]
+ * 
+ * @author Brandt
+ *
+ */
 public class Coordinate {
-	private int row;
-	private int col;
+	private final int ROW;
+	private final int COL;
 	
+	/**
+	 * Initializes a coordinate with a row and column.
+	 * 
+	 * @param ROW index of row
+	 * @param COL index of column
+	 */
 	public Coordinate(int row, int col) {
-		this.row = row;
-		this.col = col;
+		this.ROW = row;
+		this.COL = col;
 	}
 	
 	public int getRow(){
-		return row;
+		return ROW;
 	}
 	
 	public int getCol(){
-		return col;
+		return COL;
 	}
 	
+	/**
+	 * Compares 2 Coordinate objects for equality.
+	 * 
+	 * @param coordinate	compared to this.
+	 * @return				True if row and col match this, else False.
+	 */
 	public boolean equals(Coordinate coordinate) {
-		return ( row == coordinate.getRow() ) && ( col == coordinate.getCol() );
+		return ( ROW == coordinate.getRow() ) && ( COL == coordinate.getCol() );
 	}
 }
